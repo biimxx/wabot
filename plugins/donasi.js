6b00d2eb3156dd9e2d57668fcc6fc1506dccb68f
biimxx <108846100+biimxx@users.noreply.github.com>
2022-07-07 19:06:41 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-            .✵.𝗚𝗥𝗨𝗣 𝗣𝗘𝗡𝗧𝗔𝗕𝗢𝗧.✵.
+            .✵.𝗚𝗥OUP BIMbot.✵.
 
-            https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2
+            https://chat.whatsapp.com/IruqYDfFLcHBgQl2qI41PO
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
